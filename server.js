f82299d685ff72a4e65afef6cd4e598d3437256a
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2023-02-13 15:14:50 +0600

    routes added

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const projectRoute = require("./routes/projectRoute");
 
 // express app
 const app = express();
 
 // port 
 const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+})
+
+
+app.use("/app/projects", projectRoute);
 
 // listen for requests
 app.listen(port, () => {
